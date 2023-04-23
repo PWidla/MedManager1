74f1b25c7b7edd52c7a02002ca052cba46b3af8f
Patryk <patrykwidlazb@gmail.com>
2023-04-23 13:51:33 +0200

    Add role, user and its validation

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Enums;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MedicalDataManagementApp.Core.Entities
+{
+    [Table("User")]
+    public class User
+    {
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+    }
+}
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,3 +1,8 @@
+using FluentValidation;
+using MedicalDataManagementApp.Core.Entities;
+using MedicalDataManagementApp.Infrastructure.Validators;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IValidator<User>, UserValidator>();
+
 
 var app = builder.Build();
 
