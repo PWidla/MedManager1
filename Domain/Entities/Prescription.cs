533dba975728ff859e1f1a829fd84c594064c812
Patryk <patrykwidlazb@gmail.com>
2023-04-30 14:53:28 +0200

    Add prescription entity

---
+++ Domain/Entities/Prescription.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Common;
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Domain.Entities
+{
+    [Table("Prescription")]
+    public class Prescription : BaseEntity
+    {
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ExpiresAt { get; set; }
+        public string Medicines { get; set; }
+        public string Description { get; set; }
+        public Doctor Doctor { get; set; }
+        public Patient Patient { get; set; }
+    }
+}
