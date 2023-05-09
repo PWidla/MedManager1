4c8434eadc208703525039976d3aab1c57b7cf4f
Patryk <patrykwidlazb@gmail.com>
2023-05-09 15:30:32 +0200

    Add user commands

--- Application/Features/UserFeatures/Commands/CreateUserCommand.cs
+++ Application/Features/UserFeatures/Commands/CreateUserCommand.cs
@@ -1,33 +1,46 @@
-﻿//using Application.Interfaces;
-//using Domain.Entities;
-//using MediatR;
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Domain.Enums;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
 
-//namespace Application.Features.ProductFeatures.Commands
-//{
-//    public class CreateUserCommand : IRequest<int>
-//    {
-//        public string Name { get; set; }
-//        public string Barcode { get; set; }
-//        public string Description { get; set; }
-//        public decimal Rate { get; set; }
-//        public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, int>
-//        {
-//            private readonly IApplicationDbContext _context;
-//            public CreateProductCommandHandler(IApplicationDbContext context)
-//            {
-//                _context = context;
-//            }
-//            public async Task<int> Handle(CreateProductCommand command, CancellationToken cancellationToken)
-//            {
-//                var product = new Product();
-//                product.Barcode = command.Barcode;
-//                product.Name = command.Name;
-//                product.Rate = command.Rate;
-//                product.Description = command.Description;
-//                _context.Products.Add(product);
-//                await _context.SaveChangesAsync();
-//                return product.Id;
-//            }
-//        }
-//    }
-//}
+namespace Application.Features.UserFeatures.Commands
+{
+    public class CreateUserCommand : IRequest<int>
+    {
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Role Role { get; set; }
+
+        public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, int>
+        {
+            private readonly IApplicationDbContext _context;
+            public CreateUserCommandHandler(IApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<int> Handle(CreateUserCommand command, CancellationToken cancellationToken)
+            {
+                // check if user already exists
+                var existingUser = await _context.Users.FirstOrDefaultAsync(u => u.EmailAddress == command.EmailAddress);
+                if (existingUser != null)
+                {
+                    throw new Exception("User with this email already exists");
+                }
+
+                var user = new User();
+                user.EmailAddress = command.EmailAddress;
+                user.Password = command.Password;
+                user.DateOfBirth = command.DateOfBirth;
+                user.Role = command.Role;
+
+                _context.Users.Add(user);
+                await _context.SaveChangesAsync();
+                return user.Id;
+            }
+
+        }
+    }
+}
---
+++ Application/Features/UserFeatures/Commands/UpdateUserCommand.cs
@@ -0,0 +1,43 @@
+﻿using Application.Interfaces;
+using Domain.Enums;
+using MediatR;
+
+namespace Application.Features.UserFeatures.Commands
+{
+    public class UpdateUserCommand : IRequest<int>
+    {
+        public int Id { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Role Role { get; set; }
+
+        public class UpdateUserCommandHandler : IRequestHandler<UpdateUserCommand, int>
+        {
+            private readonly IApplicationDbContext _context;
+            public UpdateUserCommandHandler(IApplicationDbContext context)
+            {
+                _context = context;
+            }
+            public async Task<int> Handle(UpdateUserCommand command, CancellationToken cancellationToken)
+            {
+                var user = _context.Users.Where(a => a.Id == command.Id).FirstOrDefault();
+
+                if (user == null)
+                {
+                    return default;
+                }
+                else
+                {
+                    user.EmailAddress = command.EmailAddress;
+                    user.Password = command.Password;
+                    user.DateOfBirth = command.DateOfBirth;
+                    user.Role = command.Role;
+
+                    await _context.SaveChangesAsync();
+                    return user.Id;
+                }
+            }
+        }
+    }
+}
