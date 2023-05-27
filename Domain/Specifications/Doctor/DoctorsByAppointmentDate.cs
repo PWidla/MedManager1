b40ce10bf60ad6b6ca4223ceefbee4cfecaa0fbf
Patryk <patrykwidlazb@gmail.com>
2023-05-27 15:18:36 +0200

    A

---
+++ Domain/Specifications/Doctor/DoctorsByAppointmentDate.cs
@@ -0,0 +1,13 @@
+﻿using Application.Specification;
+
+namespace Domain.Specifications.Doctor
+{
+    public class DoctorsByAppointmentDate : BaseSpecification<Entities.Doctor>
+    {
+        public DoctorsByAppointmentDate(DateTime appointmentDate)
+            : base(d => d.Appointments != null && d.Appointments.Any(a => a.Date.Date == appointmentDate.Date))
+        {
+            AddInclude(d => d.Appointments);
+        }
+    }
+}
---
+++ Domain/Specifications/Patient/PatientsByAppointmentDate.cs
@@ -0,0 +1,18 @@
+﻿using Application.Specification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Specifications.Patient
+{
+    public class PatientsByAppointmentDate : BaseSpecification<Entities.Patient>
+    {
+        public PatientsByAppointmentDate(DateTime appointmentDate)
+            : base(d => d.Appointments != null && d.Appointments.Any(a => a.Date.Date == appointmentDate.Date))
+        {
+            AddInclude(d => d.Appointments);
+        }
+    }
+}
