1c9023a50bc312974bc5dced780e423b42eafa94
Patryk <patrykwidlazb@gmail.com>
2023-05-27 08:42:00 +0200

    Change infrastructure

---
+++ Infrastructure/Persistence/Context/ApplicationDbContext.cs
@@ -0,0 +1,49 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Patient>()
+                .HasMany(p => p.Appointments)
+                .WithOne(a => a.Patient)
+                .HasForeignKey(a => a.PatientId);
+
+            modelBuilder.Entity<Patient>()
+                .HasMany(p => p.Prescriptions)
+                .WithOne(pr => pr.Patient)
+                .HasForeignKey(pr => pr.PatientId);
+
+            modelBuilder.Entity<Doctor>()
+                .HasMany(d => d.Appointments)
+                .WithOne(a => a.Doctor)
+                .HasForeignKey(a => a.DoctorId);
+
+            modelBuilder.Entity<Doctor>()
+                .HasMany(d => d.Prescriptions)
+                .WithOne(p => p.Doctor)
+                .HasForeignKey(p => p.DoctorId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Prescription> Prescriptions { get; set; }
+        public DbSet<User> Users { get; set; }
+        public async Task<int> SaveChangesAsync()
+        {
+            return await base.SaveChangesAsync();
+        }
+    }
+}
---
+++ Infrastructure/Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,81 @@
+﻿using Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    public class EfGenericRepository<T, K> : IGenericRepository<T, K> where T : class, IIdentity<K> where K : IComparable<K>
+    {
+        private readonly DbSet<T> _dbSet;
+        private readonly ApplicationDbContext _context;
+
+        public EfGenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public IEnumerable<T> Find(ISpecification<T> specification = null)
+        {
+            var query = _dbSet.AsQueryable();
+            if (specification != null)
+            {
+                query = EFSpecificationEvaluator<T>.GetQuery(query, specification);
+            }
+            return query.ToList();
+        }
+
+        public async Task<T?> FindByIdAsync(K id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<List<T>> FindAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public T? FindById(K id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public List<T> FindAll()
+        {
+            return _dbSet.ToList();
+        }
+
+        public T Add(T entity)
+        {
+            _dbSet.Add(entity);
+            return entity;
+        }
+
+        public void RemoveById(K id)
+        {
+            var entity = _dbSet.Find(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+            }
+        }
+
+        public void Update(K id, T entity)
+        {
+            var existingEntity = _dbSet.Find(id);
+            if (existingEntity != null)
+            {
+                _context.Entry(existingEntity).CurrentValues.SetValues(entity);
+            }
+        }
+
+        public IEnumerable<T> FindBySpecification(ISpecification<T> specification = null)
+        {
+            var query = _dbSet.AsQueryable();
+            if (specification != null)
+            {
+                query = EFSpecificationEvaluator<T>.GetQuery(query, specification);
+            }
+            return query.ToList();
+        }
+    }
+}
