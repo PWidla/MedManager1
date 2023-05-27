5ac0764aeee19edc3004cde56bb4973f4594beba
Patryk <patrykwidlazb@gmail.com>
2023-05-27 18:23:39 +0200

    Add admin validator

---
+++ Domain/Validators/AdminValidator.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using FluentValidation;
+
+namespace Domain.Validators
+{
+    public class AdminValidator : AbstractValidator<Admin>
+    {
+        public AdminValidator()
+        {
+            RuleFor(a => a.Nickname)
+                .NotEmpty().WithMessage("Nickname address is required.")
+                .MinimumLength(8).WithMessage("Nickname must be at least 3 characters long.");
+        }
+    }
+}
